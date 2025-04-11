f3ff5d4343a7ec99b65d5accd777b180ac8fed09
Moonshammy <benjaminmooneyham1@gmail.com>
2025-04-10 21:05:49 -0600

    Bullets added

--- shot.py
+++ shot.py
@@ -1,12 +1,9 @@
 import pygame
 from circleshape import CircleShape
 
-class Asteroid(CircleShape):
+class Shot(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-
-    def split(self):
-        self.kill()
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
